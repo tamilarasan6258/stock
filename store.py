b1be0dc712c8981904cb3e3b5f2dd67d4d1a33dc
tamilarasan6258 <139328394+tamilarasan6258@users.noreply.github.com>
2023-11-28 14:13:52 +0530

    Update store.py

--- store.py
+++ store.py
@@ -4,12 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    """Return a friendly HTTP greeting.
-
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    return "Hello World!"
+ 
+    return Product
 if __name__ == "__main__":
    
     app.run(host="127.0.0.1", port=8080, debug=True)
