0c1a3755841abfd44b8d1b768e9bbc3e75be55d6
tamilarasan6258 <139328394+tamilarasan6258@users.noreply.github.com>
2023-11-28 14:08:28 +0530

    Update store.py

--- store.py
+++ store.py
@@ -10,6 +10,10 @@ def hello():
         A string with the words 'Hello World!'.
     """
     return "Hello World!"
+if __name__ == "__main__":
+   
+    app.run(host="127.0.0.1", port=8080, debug=True)
+
 
 
 
@@ -224,11 +228,5 @@ while True:
         print("Invalid choice! Please try again.")
 
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+
 
